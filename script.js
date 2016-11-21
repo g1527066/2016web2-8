c24f2ab0283f28bf94f44bfa7d5a1124e6526cc0
g1527066 <g1527066@st.t-kougei.ac.jp>
2016-11-21 15:58:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,6 @@ onload = function(){
   c.height = 500;
 	
 var gl=canvas.getContext('webgl')||canvas.getContext('experimental-webgl');
-	gl.clearColor(0.0,0.0,0.0,1.0);
+	gl.clearColor(1.0,0.0,0.0,1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 };
