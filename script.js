492ecd1b7d53b570acc48e4f3e5222e52ab7ee93
g1527066 <g1527066@st.t-kougei.ac.jp>
2016-11-21 15:57:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ onload = function(){
   // canvasエレメントを取得
   var c = document.getElementById('canvas');
   c.width = 500;
-  c.height = 300;
+  c.height = 500;
 	
 var gl=canvas.getContext('webgl')||canvas.getContext('experimental-webgl');
 	gl.clearColor(0.0,0.0,0.0,1.0);
